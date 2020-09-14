2ae6784ea7bd34e1545e65f3c29734daea1efc0b
evan holt <adiga_knight@hotmail.com>
2020-09-14 19:01:19 +0300

    new get-found-person lambda

---
+++ Backend/lambda/lambda-functions/found/get-found-person/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const connectionCheck = require('/opt/nodejs/checkConnection.js')(process.env.DB_CONN);
+let FoundPerson = require('/opt/nodejs/models/FoundPerson.js');
+
+exports.handler = async (event,context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    await connectionCheck;
+    return FoundPerson.findById(event.params.path.id);
+};
