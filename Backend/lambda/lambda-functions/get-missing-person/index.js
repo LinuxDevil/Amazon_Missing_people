662969102ba359a1ef0521b59fd6f26e9a5a5c44
evan holt <adiga_knight@hotmail.com>
2020-09-14 16:46:54 +0300

    new get-missing-person lambda

---
+++ Backend/lambda/lambda-functions/get-missing-person/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const connectionCheck = require('/opt/nodejs/checkConnection.js')(process.env.DB_CONN);
+let MissingPerson = require('/opt/nodejs/models/MissingPerson.js');
+
+exports.handler = async (event,context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    await connectionCheck;
+    return MissingPerson.findById(event.params.path.id);
+};
