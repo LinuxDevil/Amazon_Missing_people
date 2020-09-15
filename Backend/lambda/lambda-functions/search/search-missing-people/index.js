26c3940d5307e90a68e46f38e893a7b6f127e365
evan holt <adiga_knight@hotmail.com>
2020-09-16 00:14:05 +0300

    fully-functional search!!

--- Backend/lambda/lambda-functions/search/search-missing-people/index.js
+++ Backend/lambda/lambda-functions/search/search-missing-people/index.js
@@ -5,52 +5,79 @@ let MissingPerson = require('/opt/nodejs/models/MissingPerson.js');
 exports.handler = async (event,context) => {
     context.callbackWaitsForEmptyEventLoop = false;
     await connectionCheck;
-
-    const result = await MissingPerson.aggregate([
+    
+    
+    let resultLimit = 2;
+    let resultsToSkip = 0;
+    if(event.params) {
+      resultsToSkip = event.params.path.page ? (event.params.path.page - 1) * resultLimit : 0;
+    }
+      
+    
+    // base query
+    let query = [
       {
         $search: {
           "compound": {
-            "should": [
-              {
+            "should": [],
+              "minimumShouldMatch": 1
+          }
+        },
+      },
+      { $skip  : resultsToSkip  },
+      { $limit : resultLimit }
+      
+    ];
+    
+    // adds to query based on availability
+    if(event.body.name) {
+      query[0].$search.compound.should.push({
                 "text": {
-                  "query": event.name,
+                  "query": event.body.name,
                   "path": "name",
                   "fuzzy": {
                    "maxEdits": 2,
                   }
                 }
-              },
-              {
+              });
+    }
+    
+    if(event.body.mother_name) {
+      query[0].$search.compound.should.push({
                 "text": {
-                  "query": event.mother_name,
+                  "query": event.body.mother_name,
                   "path": "mother_name",
                   "fuzzy": {
                    "maxEdits": 2,
                   }
                 }
-              },
-              {
+              });
+    }
+    
+    if(event.body.national_number) {
+      query[0].$search.compound.should.push({
                 "range": {
-                  "gte": event.national_number,
-                  "lte": event.national_number,
+                  "gte": event.body.national_number,
+                  "lte": event.body.national_number,
                   "path": "national_number",
                   "score": { "boost": { "value": 5 } }
                 }
-              },
-              {
+              });
+    }
+    
+    if(event.body.gender) {
+      query[0].$search.compound.should.push({
                 "text": {
-                  "query": event.gender,
+                  "query": event.body.gender,
                   "path": "gender",
                   "score": { "boost": { "value": 2 } }
                 }
-              }
-              ],
-              "minimumShouldMatch": 1
-            }
-          }
-        }
-    ])
+              });
+    }
+    
+    const result = await MissingPerson.aggregate(query)
 
     if(result.length == 0) return "No Result Found.";
+    
     return result;
 };
