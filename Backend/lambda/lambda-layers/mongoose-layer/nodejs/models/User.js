43c00ccfb4fcd6ff1c9be1df189f327aac062bb7
evan holt <adiga_knight@hotmail.com>
2020-09-16 19:18:03 +0300

    change to phone number structure on user model

--- Backend/lambda/lambda-layers/mongoose-layer/nodejs/models/User.js
+++ Backend/lambda/lambda-layers/mongoose-layer/nodejs/models/User.js
@@ -8,7 +8,7 @@ const userSchema = new Schema ({
     required:true,
     validate: {
       validator: function(v) {
-        return /\d{3}-\d{3}-\d{4}/.test(v);
+        return /\d{10}/.test(v);
       },
       message: props => `${props.value} is not a valid phone number!`
     }
